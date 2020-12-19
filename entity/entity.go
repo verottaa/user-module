a162d6e7c7818cdf13118b3d4d5ecfe08bc54383
urglmorl <eriksongerson@yandex.ru>
2020-12-20 00:30:04 +0300

    Интеграция поиска

--- entity/entity.go
+++ entity/entity.go
@@ -1,6 +1,10 @@
 package entity
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type User struct {
 	Id         primitive.ObjectID `json:"id" bson:"_id"`
@@ -13,4 +17,29 @@ type User struct {
 	Branch     primitive.ObjectID `json:"branch" bson:"branch"`
 	Department primitive.ObjectID `json:"department" bson:"department"`
 	Position   primitive.ObjectID `json:"position" bson:"position"`
+}
+
+type UserFilter struct {
+	Id         primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	Name       string             `json:"name" bson:"name,omitempty"`
+	Surname    string             `json:"surname" bson:"surname,omitempty"`
+	Patronymic string             `json:"patronymic" bson:"patronymic,omitempty"`
+	Email      string             `json:"email" bson:"email,omitempty"`
+	Password   string             `json:"password" bson:"password,omitempty"`
+	Type       primitive.ObjectID `json:"type" bson:"type,omitempty"`
+	Branch     primitive.ObjectID `json:"branch" bson:"branch,omitempty"`
+	Department primitive.ObjectID `json:"department" bson:"department,omitempty"`
+	Position   primitive.ObjectID `json:"position" bson:"position,omitempty"`
+}
+
+func (u User) GetUserFromBson(data interface{}) error {
+	m, _ := bson.Marshal(data)
+
+	err := bson.Unmarshal(m, &u)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	return nil
 }
--- repository/repositoryImpl.go
+++ repository/repositoryImpl.go
@@ -1,6 +1,9 @@
 package repository
 
 import (
+	"errors"
+	"fmt"
+	database_module "github.com/verottaa/database-module"
 	"github.com/verottaa/user-module/entity"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"sync"
@@ -8,10 +11,12 @@ import (
 
 var once sync.Once
 var destroyCh = make(chan bool)
+var getCollectionNameCh = make(chan chan string)
 
 var repositoryInstance *repository
 
 type repository struct {
+	collectionName string
 }
 
 func GetRepository() Repository {
@@ -21,6 +26,8 @@ func GetRepository() Repository {
 			for
 			{
 				select {
+				case ch := <-getCollectionNameCh:
+					ch <- repositoryInstance.collectionName
 				case <-destroyCh:
 					return
 				}
@@ -41,8 +48,36 @@ func (r repository) Destroy() {
 	repositoryInstance = nil
 }
 
+func (r repository) GetCollectionName() string {
+	resCh := make(chan string)
+	defer close(resCh)
+	getCollectionNameCh <- resCh
+	return <-resCh
+}
+
 func (r repository) Find(id primitive.ObjectID) (*entity.User, error) {
-	panic("implement me")
+	var filter = entity.UserFilter{
+		Id: id,
+	}
+	code, bsonUser := database_module.FindOne(r.GetCollectionName(), filter)
+	switch code {
+	case database_module.FOUND:
+		return decodeUserFromBson(bsonUser)
+	case database_module.ERROR:
+		return nil, errors.New("unexpected error with database connection")
+	default:
+		return nil, nil
+	}
+}
+
+func decodeUserFromBson(bsonUser interface{}) (*entity.User, error) {
+	var user entity.User
+	err := user.GetUserFromBson(bsonUser)
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil, err
+	}
+	return &user, nil
 }
 
 func (r repository) FindAll() ([]*entity.User, error) {
